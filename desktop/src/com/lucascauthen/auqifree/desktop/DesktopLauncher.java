8e7f89cdc165cb12c891b6600c25efbafbead2b5
lucascauthen <lucascauthen@gmail.com>
2015-04-06 23:30:18 -0500

    Initial Commit
    Adding the auto generated files from libgdx

---
+++ desktop/src/com/lucascauthen/auqifree/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lucascauthen.auqifree.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lucascauthen.auqifree.AquifreeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AquifreeGame(), config);
+	}
+}
